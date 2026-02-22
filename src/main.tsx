5f710ea6c6a4dc7608b5f547a6a2c61aa7955c82
miyachin <miyachin@MacBook-Pro-M1-Pro.local>
2026-02-22 20:18:19 +0900

    Auto deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import './index.css'
 import './i18n'; // Initialize i18n
 import App from './App.tsx'
 
+import { Analytics } from '@vercel/analytics/react';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
